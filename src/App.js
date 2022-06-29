43c61726e31ef971f061e2d9bf413e99a45c6c52
163002029.junayed <junayednoor@gmail.com>
2022-06-29 11:22:57 +0600

    Action Dispatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const decrement = () => {
+    dispatch({
+      type: 'DEC',
+    });
+  };
+  const increment = () => {
+    dispatch({
+      type: 'INC',
+    });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment} style={{ padding: '10px', borderRadius: '10px', margin: '10px' }}>
+        Increment
+      </button>
+      <button onClick={decrement} style={{ padding: '10px', borderRadius: '10px', margin: '10px' }}>
+        Decrement
+      </button>
     </div>
   );
 }
